49607e1e5bc3af1123ac6ea1ff82e28deb190b73
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-07-31 09:30:05 -0700

    hardcode weth as holding position for RYE (#17)
    * hardcode weth as holding position for RYE
    
    * leave a comment as to why rye holding position changed

--- subgraphs/cellars/src/utils/v2.ts
+++ subgraphs/cellars/src/utils/v2.ts
@@ -10,6 +10,8 @@ import {
   ZERO_BI,
   TEN_BI,
   NEGATIVE_ONE_BI,
+  REAL_YIELD_ETH,
+  WETH_ADDRESS,
 } from "./constants";
 import {
   convertDecimals,
@@ -96,7 +98,15 @@ export function snapshotDay(
     const holdingPosition = getHoldingPosition(contract);
 
     if (holdingPosition != Address.zero()) {
-      const holdingContract = ERC20.bind(holdingPosition);
+      // FIXME:
+      // remove this hack or create a mapping of cellar -> holding positions
+      // instead of getting it dynamically. Historically the holding position
+      // has never changed until we needed to block deposits to RYE.
+      let holdingContract = ERC20.bind(holdingPosition);
+      if (cellarAddress == REAL_YIELD_ETH) {
+        holdingContract = ERC20.bind(WETH_ADDRESS);
+      }
+
       const holdingBalance = holdingContract.balanceOf(address);
 
       const holdingAsset = loadOrCreateTokenERC20(
